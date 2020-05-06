53833df17c2d30e30b36245be65c89092367b6af
Laurent Beaudoux <lbdx@x55vd.home>
2020-05-06 18:53:30 +0200

    improve speed

--- tatoebatools/parallel_corpora.py
+++ tatoebatools/parallel_corpora.py
@@ -1,22 +1,63 @@
+import logging
+
 from .corpus import Corpus
 from .links import Links
+from .utils import lazy_property
 
 
 class ParallelCorpora:
-    """
+    """A parallel corpora, i.e. a collection of sentences placed alongside
+    their translations
     """
 
-    def __init__(self, source_language, target_language):
+    def __init__(self, source_language_code, target_language_code):
+        # the source language of the parallel corpora
+        self._src_lg = source_language_code
+        # the target language of the parallel corpora
+        self._tgt_lg = target_language_code
+        # the corpus instance containing the source sentences
+        self._src_corpus = Corpus(self._src_lg)
+        # the corpus instance containing the target sentences
+        self._tgt_corpus = Corpus(self._src_lg)
+        # the links instance containing the links between sentences
+        self._links = Links(self._src_lg, self._tgt_lg)
 
-        self._src_lg = source_language
-        self._tgt_lg = target_language
+        # log a warning if the datafiles don't share a common version
+        if not (
+            self._src_corpus.version.date()
+            == self._tgt_corpus.version.date()
+            == self._tgt_corpus.version.date()
+        ):
+            logging.warning(
+                "the versions of the data files difer. an update is advised."
+            )
 
     def __iter__(self):
 
-        src_corpus = {s.id: s for s in Corpus(self._src_lg)}
-        tgt_corpus = {s.id: s for s in Corpus(self._tgt_lg)}
+        # load useful sentences from corpora
+        logging.info(f"loading {self._src_lg} sentences")
+        sentences = {
+            s.id: s for s in Corpus(self._src_lg) if s.id in self.sentence_ids
+        }
 
-        links = Links(self._src_lg, self._tgt_lg)
+        logging.info(f"loading {self._tgt_lg} sentences")
+        translations = {
+            s.id: s
+            for s in Corpus(self._tgt_lg)
+            if s.id in self.translation_ids
+        }
 
-        for lk in links:
-            yield (src_corpus[lk.sentence_id], tgt_corpus[lk.translation_id])
+        for lk in self._links:
+            yield (sentences[lk.sentence_id], translations[lk.translation_id])
+
+    @lazy_property
+    def sentence_ids(self):
+        """Get the ids of the source sentences.
+        """
+        return self._links.sentence_ids
+
+    @lazy_property
+    def translation_ids(self):
+        """Get the ids of the target translations.
+        """
+        return self._links.translation_ids
