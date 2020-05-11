c0535dd4b9200907ec09ad76546ff33341ce4dad
Laurent Beaudoux <lbdx@x55vd.home>
2020-05-11 22:02:02 +0200

    improve update

--- tatoebatools/__init__.py
+++ tatoebatools/__init__.py
@@ -9,44 +9,60 @@ to_index = ("sentences_detailed.tsv", "sentences.tsv", "sentences_CC0.tsv")
 
 
 class Tatoeba:
-    """
+    """A handler for managing Tatoeba data on the client side.
     """
 
     def __init__(self, *language_codes):
-
+        # the languages for which some Tatoeba data is required
         self._lgs = language_codes
 
     def update(self, *table_names):
-        """
+        """Update the tables and classify them by required language.
         """
         # datafile containing sentences has a priority because it is
         # necessary for splitting files by language
-        if any(
-            tbl in ("sentences", "sentences_detailed") for tbl in table_names
-        ):
-            table_names = sorted(
-                table_names,
-                key=lambda x: x in ("sentences", "sentences_detailed"),
-                reverse=True,
-            )
-        else:
-            table_names.insert(0, "sentences")
+        # if any(tbl == "sentences_detailed" for tbl in table_names):
+        #     table_names = sorted(
+        #         table_names,
+        #         key=lambda x: x == "sentences_detailed",
+        #         reverse=True,
+        #     )
+        # else:
+        #     table_names.insert(0, "sentences_detailed")
 
-        updated_tables = []
+        sentences_tbl = Table("sentences_detailed", self._lgs)
+        if sentences_tbl.update():
+            updated_tables = ["sentences_detailed"]
+        else:
+            updated_tables = []
+
         for tbl_name in table_names:
+            if tbl_name == "sentences_detailed":  # already done
+                continue
+
             tbl = Table(tbl_name, self._lgs)
             if tbl.update():
                 updated_tables.append(tbl_name)
 
-                if tbl.name in (
-                    "tags",
-                    "sentences_in_lists",
-                    "jpn_indices",
-                    "sentences_with_audio",
-                    "user_languages",
-                    "queries",
-                ):
-                    tbl.classify(self.lang_index)
+            if tbl_name in updated_tables and tbl_name in (
+                "links",
+                "tags",
+                "sentences_in_lists",
+                "jpn_indices",
+                "sentences_with_audio",
+                "user_languages",
+                "queries",
+            ):
+                tbl.classify(self.language_index)
+
+            elif "sentences_detailed" in updated_tables and tbl_name in (
+                "links",
+                "tags",
+                "sentences_in_lists",
+                "jpn_indices",
+                "sentences_with_audio",
+            ):  
+                tbl.classify(self.language_index)
 
         if updated_tables:
             logging.info("{} updated".format(", ".join(updated_tables)))
@@ -57,21 +73,15 @@ class Tatoeba:
     def language_index(self):
         """Get the index that maps the sentences' ids to their language.
         """
-        tbl_names = ("sentences", "sentences_detailed")
-        sentence_tbl = Table("sentences", self._lgs)
+        logging.info("mapping sentence ids to languages")
 
-        return {
-            id: lg
-            for df in sentence_tbl.language_detafiles
-            for id, lg in Index(df, 0, 1)
-        }
+        return get_language_index(*self._lgs)
 
     @property
     def downloadable_tables(self):
         """
         """
         return [
-            "sentences",
             "sentences_detailed",
             "sentences_CC0",
             "transcriptions",
@@ -84,3 +94,15 @@ class Tatoeba:
             "user_languages",
             "queries",
         ]
+
+
+def get_language_index(*languages):
+    """
+    """
+    sentence_tbl = Table("sentences_detailed", languages)
+
+    return {
+        id: lg
+        for df in sentence_tbl.language_detafiles
+        for id, lg in Index(df, 0, 1)
+    }
