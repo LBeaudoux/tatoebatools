9a8ed41a3cb8cfb6a9b456348f939e1ee03e357f
lbdx <lbdx@x55vd.home>
2020-05-22 19:45:47 +0200

    import ParallelCorpora from  __init__.py

--- tatoebatools/__init__.py
+++ tatoebatools/__init__.py
@@ -1,7 +1,9 @@
 import logging
 
 from .tatoeba import Tatoeba
+from .parallel_corpora import ParallelCorpora
 
 tatoeba = Tatoeba()
+ParallelCorpora = ParallelCorpora
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
