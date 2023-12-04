6d739ea113e0c4f0a4ccbb49f462f54ed2546dde
lbdx <lbeaudoux@gmail.com>
2023-12-04 14:30:09 +0100

    Update imports for SQLAlchemy>=2.0
    The sqlalchemy.ext.declarative package is mostly moved to the
    sqlalchemy.orm package.

--- tatoebatools/models.py
+++ tatoebatools/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, DateTime, Integer, String, Text
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
