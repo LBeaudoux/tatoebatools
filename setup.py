8b7add3034b0673cb4af1ab95affdd70d8fca3bf
lbdx <lbeaudoux@gmail.com>
2023-12-05 14:20:29 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
